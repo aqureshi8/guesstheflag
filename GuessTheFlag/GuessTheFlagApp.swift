2835657ba4b4f4c1fb8b4afbf6ffdba3f89bffac
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-11 19:40:54 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ahsan Qureshi on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
